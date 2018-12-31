1a63d77c6901204925158d68a14c04a87d875303
Motaz Saad <motaz.saad@gmail.com>
2018-12-31 10:01:38 +0200

    fix config

--- test_web.py
+++ test_web.py
@@ -25,6 +25,13 @@ def extractor():
     answer = answer + '\n<p><b>text:</b>\n' + text + '/<p>'
     return answer
 
+# sudo ufw enable
+# sudo ufw allow 5000/tcp
+# sudo ufw status verbose
+# app.run(host='0.0.0.0' , port=5000)
 
 if __name__ == '__main__':
-    app.run(debug=True, host=gethostname())
+	print(gethostname())
+    # app.run(debug=True, host=gethostname(), port=5000)
+	# app.run(debug=True, host='0.0.0.0', port=5000)
+	app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
