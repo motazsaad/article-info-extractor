a904c07cb2439afcebeddb8601e22fb34d3db06a
Motaz Saad <motaz.saad@gmail.com>
2019-01-02 05:17:02 +0000

    run app env

--- flask_app.py
+++ flask_app.py
@@ -43,6 +43,10 @@ def extractor():
 
 if __name__ == '__main__':
 	print(gethostname())
-    # app.run(debug=True, host=gethostname(), port=5000)
-	# app.run(debug=True, host='0.0.0.0', port=5000)
-	app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
+	#################################
+	# for pythonanywhere 
+	app.run() 
+    #################################
+    # for c9.io 
+	# app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
+	#################################
