19dceef5d2b74105cf751bad53bd5c28d2bd4379
Motaz Saad <motaz.saad@gmail.com>
2019-03-02 19:34:28 +0000

    improve templates, homepage, exceptions

--- extract_util.py
+++ extract_util.py
@@ -11,25 +11,53 @@ from alphabet_detector import AlphabetDetector
 from newspaper import Article
 from newspaper import fulltext
 from newsplease import NewsPlease
+import pextract as pe
+from urllib.parse import urljoin
 
-def get_title(url):
+
+def get_title_text_BS4(url):
     try:
         html_doc = requests.get(url)
         status = html_doc.status_code
         if status is 200:
             soup_doc = BeautifulSoup(html_doc.text, 'html.parser')
             title = soup_doc.title.text
+            text = soup_doc.get_text()
+            return title, text 
         else:
             title = 'connection error: {} {}'.format(str(status), responses[status])
+            text = 'connection error: {} {}'.format(str(status), responses[status])
+            return title, text 
     except RequestException as error:
-        title = 'Request error: {}'.format(error) 
-        print('Request error: {}'.format(error))
+        message = 'Request error: {}'.format(error) 
+        message, message 
     except BaseException as error:
-        title = 'error: {}'.format(error)
-        print('error: {}'.format(error))
-    return title
+        message = 'Request error: {}'.format(error) 
+        return message, message 
     
+
+def get_pextract(url):
+    try:
+        response = requests.get(url)
+        status = response.status_code
+        if status is 200:
+            soup = BeautifulSoup(response.content, 'lxml')
+            for img in soup.findAll('img'):
+            	img['src'] = urljoin(url, img['src'])
+            html, pval = pe.extract(soup, text_only = False, remove_img = False)
+            text, pval = pe.extract(soup)
+            return text
+        else:
+            message = 'connection error: {} {}'.format(str(status), responses[status])
+            return message 
+    except RequestException as error:
+        message = 'Request error: {}'.format(error) 
+        message
+    except BaseException as error:
+        message = 'Request error: {}'.format(error) 
+        return message
     
+
 def get_date(url):
     try:
         article_date = articleDateExtractor.extractArticlePublishedDate(url)
@@ -67,27 +95,20 @@ def get_text_boilerpipe(url):
     return extracted_text
     
     
-def get_text_justext(url):
-    alpha_det = AlphabetDetector()
+def get_text_justext(url, lang):
     try:
         extracted_text = ''
         response = requests.get(url)
         contents = response.content
         status = response.status_code
-        if status is 200:
-            soup_doc = BeautifulSoup(response.text, 'html.parser')
-            title = soup_doc.title.text
+        if status == 200:
+            paragraphs = justext.justext(contents, justext.get_stoplist(lang))
+            for paragraph in paragraphs:
+                # print(type(paragraph))
+                if not paragraph.is_boilerplate:
+                    extracted_text += paragraph.text + '\n'
         else:
-            title = 'connection error: {} {}'.format(str(status), responses[status])
-            return title
-        if 'ARABIC' in alpha_det.detect_alphabet(title):
-            paragraphs = justext.justext(contents, justext.get_stoplist('Arabic'))
-        else:
-            paragraphs = justext.justext(contents, justext.get_stoplist('English'))
-        for paragraph in paragraphs:
-            # print(type(paragraph))
-            if not paragraph.is_boilerplate:
-                extracted_text += paragraph.text + '\n'
+            return 'connection error: {} {}'.format(str(status), responses[status])
     except RequestException as error:
         extracted_text = 'error: {}'.format(error) 
         print('error: {}'.format(error))
@@ -95,21 +116,41 @@ def get_text_justext(url):
     
     
 def extract_newspaper3k(url):
-    article = Article(url)
-    article.download()
-    article.parse()
-    authors = article.authors
-    date = article.publish_date 
-    text = article.text
-    title = article.title
-    print ('newspaper3k title:', title)
-    print ('newspaper3k authors:', authors)
-    print ('newspaper3k date:', date)
-    return  title, text
+    try:
+        article = Article(url)
+        article.download()
+        article.parse()
+        authors = article.authors
+        date = article.publish_date 
+        text = article.text
+        title = article.title
+        return  title, text
+    except BaseException as error:
+        message = 'error: {}'.format(error)
+        return message, message 
+    
+def extract_newspaper3k_v2(url):
+    try: 
+        article = Article(url)
+        article.download()
+        article.parse()
+        article.nlp()
+        return article
+    except BaseException as error:
+        return 'error: {}'.format(error)
+    
     
 def extract_news_please(url):
-    article = NewsPlease.from_url(url)
-    title = article.title 
-    text = article.text
-    print('news-please title:', title)
-    return text 
+    try:
+        article = NewsPlease.from_url(url)
+        title = article.title 
+        text = article.text
+        # print('news-please title:', title)
+        return text 
+    except BaseException as error: 
+        return 'error: {}'.format(error)
+    
+def refine_Arabic_keywords(keywords):
+    stopwords = open('stopwords.ar').read().splitlines()
+    my_keywords = [key for key in keywords if key not in stopwords]
+    return '\t'.join(my_keywords)
--- flask_app.py
+++ flask_app.py
@@ -5,43 +5,63 @@ from socket import gethostname
 import os 
 from alphabet_detector import AlphabetDetector
 from flask import render_template
+from collections import OrderedDict
+
 
 
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/index')
 def welcome():
-    return '<h1>welcome :)</h1>'
+    urls = open('urls.txt').read().split('\n')
+    return render_template('home.html', urls=urls)
 
 
 @app.route('/extractor')
 def extractor():
     alpha_det = AlphabetDetector()
     url = request.args.get('url')
+    if not url:
+        return render_template('no_url.html')
     url = url.strip()
     # title = extract_util.get_title(url)
     title, newspaper3k_text = extract_util.extract_newspaper3k(url)
-    date = extract_util.get_date(url)
-    text_boilerpipe = extract_util.get_text_boilerpipe(url)
-    text_justext = extract_util.get_text_justext(url)
-    news_please_text = extract_util.extract_news_please(url)
-
-    text_boilerpipe = text_boilerpipe.split('\n')
-    text_justext = text_justext.split('\n')
-    newspaper3k_text = newspaper3k_text.split('\n')
-    news_please_text = news_please_text.split('\n')
     
     if 'ARABIC' in alpha_det.detect_alphabet(title):
         text_dir = 'rtl'
+        lang = 'Arabic'
     else:
         text_dir = 'ltr'
-    return render_template('article_info.html', url=url, title=title, date=date, text_dir=text_dir, text_boilerpipe=text_boilerpipe, text_justext=text_justext, newspaper3k_text=newspaper3k_text, news_please_text=news_please_text)
+        lang = 'English'
     
-# sudo ufw enable
-# sudo ufw allow 5000/tcp
-# sudo ufw status verbose
-# app.run(host='0.0.0.0' , port=5000)
+    date = extract_util.get_date(url)
+    text_boilerpipe = extract_util.get_text_boilerpipe(url)
+    text_justext = extract_util.get_text_justext(url, lang)
+    news_please_text = extract_util.extract_news_please(url)
+    # _, bs4_text = extract_util.get_title_text_BS4(url)
+    text_pextract = extract_util.get_pextract(url)
+    
+    texts = OrderedDict()
+    text_boilerpipe = text_boilerpipe.split('\n')
+    texts['Boilerpipe'] = text_boilerpipe
+    text_justext = text_justext.split('\n')
+    texts['Justext'] = text_justext
+    newspaper3k_text = newspaper3k_text.split('\n')
+    texts['Newspaper3k'] = newspaper3k_text
+    news_please_text = news_please_text.split('\n')
+    texts['NewsPlease'] = news_please_text
+    # bs4_text = bs4_text.split('\n')
+    # texts['BeautifulSoup'] = bs4_text
+    text_pextract = text_pextract.split('\n')
+    texts['pextract'] = text_pextract
+    
+    
+    
+    return render_template('article_info.html', url=url, title=title, date=date, text_dir=text_dir, texts=texts)
+    
+
 
 if __name__ == '__main__':
 	print(gethostname())
@@ -53,3 +73,10 @@ if __name__ == '__main__':
     # for c9.io 
 	app.run(debug=True, host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
 	#################################
+	
+	
+	
+# sudo ufw enable
+# sudo ufw allow 5000/tcp
+# sudo ufw status verbose
+# app.run(host='0.0.0.0' , port=5000)
