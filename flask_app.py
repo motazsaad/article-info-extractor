8511ca02a68919c8d7207babfd5ab1999c300531
Motaz Saad <motaz.saad@gmail.com>
2019-04-01 22:41:13 +0300

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -67,11 +67,11 @@ if __name__ == '__main__':
 	print(gethostname())
 	#################################
 	# for pythonanywhere
-    # if 'liveconsole' not in gethostname():
-    # app.run()
-    #################################
-    # for c9.io 
-	app.run(debug=True, host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
+	# if 'liveconsole' not in gethostname():
+	app.run()
+	#################################
+	# for c9.io 
+	# app.run(debug=True, host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
 	#################################
 	
 	
